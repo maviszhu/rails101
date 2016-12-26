2d80f78dc7e8c42fedc8578ae5a85f16fb3d6fab
maviszhu <maviszhu1987@gmail.com>
2016-12-26 21:43:38 +0800

    use before_action to find_group_and_cbeck_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
   @groups = Group.all
 end
@@ -10,10 +11,6 @@ def show
   @group = Group.find(params[:id])
 end
 def edit
-  @group = Group.find(params[:id])
-  if current_user != @group.user
-    redirect_to root_path, alert: "You have no permission."
-  end
 end
 def create
   @group = Group.new(group_params)
@@ -25,10 +22,6 @@ def create
   end
 end
 def update
-  @group = Group.find(params[:id])
-  if current_user != @group.user
-    redirect_to root_path, alert: "You have no permission."
-  end
   if @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
   else
@@ -36,10 +29,6 @@ def update
   end
 end
 def destroy
-  @group = Group.find(params[:id])
-  if current_user != @group.user
-    redirect_to root_path, alert: "You have no permission."
-  end
   @group.destroy
   flash[:alert] = "Group deleted"
   redirect_to groups_path
@@ -47,5 +36,11 @@ end
 private
 def group_params
   params.require(:group).permit(:title, :description)
+end
+def find_group_and_check_permission
+  @group = Group.find(params[:id])
+  if current_user != @group.user
+    redirect_to root_path, alert: "You have no permission."
+  end
 end
 end
